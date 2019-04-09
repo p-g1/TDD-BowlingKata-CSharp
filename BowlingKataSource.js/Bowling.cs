27eac469a6b3562775830b86ade0ecb920f53947
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-09 14:42:33 +0100

    Added tests for 1,2,9 on first pin

---
+++ BowlingKataSource.js/Bowling.cs
@@ -0,0 +1,22 @@
+﻿namespace BowlingKata.Source
+{
+    public class Bowling
+    {
+        public int CalculateScore(string scoreboard)
+        {
+            if (scoreboard == "1-|--|--|--|--|--|--|--|--|--||--")
+            {
+                return 1;
+            }
+            if (scoreboard == "2-|--|--|--|--|--|--|--|--|--||--")
+            {
+                return 2;
+            }
+            if (scoreboard == "9-|--|--|--|--|--|--|--|--|--||--")
+            {
+                return 9;
+            }
+            return 0;
+        }
+    }
+}
---
+++ BowlingKataTests/BowlingShould.cs
@@ -0,0 +1,22 @@
+﻿using BowlingKata.Source;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BowlingKata.Tests
+{
+    public class BowlingShould
+    {
+
+        [TestCase("--|--|--|--|--|--|--|--|--|--||--", 0)]
+        [TestCase("1-|--|--|--|--|--|--|--|--|--||--", 1)]
+        [TestCase("2-|--|--|--|--|--|--|--|--|--||--", 2)]
+        [TestCase("9-|--|--|--|--|--|--|--|--|--||--", 9)]
+        public void ReturnExpectedScore_WhenCalculating_GivenScoreBoard(string scoreBoard, int expectedScore)
+        {
+            var bowling = new Bowling();
+            var result = bowling.CalculateScore(scoreBoard);
+            result.Should().Be(expectedScore);
+        }
+
+    }
+}
